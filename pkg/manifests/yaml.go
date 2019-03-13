08f8f1a7e12a23d4b6d492c98a9263b9d239bdb9
Jim Crossley <jim@crossleys.org>
2019-03-12 20:11:16 -0400

    Parse a hard-coded file and apply its resources
    Still need to...
    
    handle deletion, e.g. ownerRefs, finalizer, etc?
    
    un-hardcode filename
    
    deal with namespaces, roles, bindings, crds?

---
+++ pkg/manifests/yaml.go
@@ -0,0 +1,111 @@
+package manifests
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/dynamic"
+)
+
+// Parse a yaml file into a slice of Unstructured resources
+func Parse(filename string) []unstructured.Unstructured {
+	in, out := make(chan []byte, 10), make(chan unstructured.Unstructured, 10)
+	go read(filename, in)
+	go decode(in, out)
+	result := []unstructured.Unstructured{}
+	for spec := range out {
+		result = append(result, spec)
+	}
+	return result
+}
+
+// Apply a slice of Unstructured resources
+func Create(resources []unstructured.Unstructured, dc dynamic.Interface) error {
+	for _, spec := range resources {
+		c, err := client(spec, dc)
+		if err != nil {
+			return err
+		}
+		_, err = c.Create(&spec, v1.CreateOptions{})
+		if err != nil {
+			fmt.Println("ERROR", spec.GetName(), err)
+		}
+	}
+	return nil
+}
+
+func buffer(file *os.File) []byte {
+	var size int64 = bytes.MinRead
+	if fi, err := file.Stat(); err == nil {
+		size = fi.Size()
+	}
+	return make([]byte, size)
+}
+
+func read(filename string, sink chan []byte) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err.Error())
+	}
+	manifests := yaml.NewDocumentDecoder(file)
+	defer manifests.Close()
+	buf := buffer(file)
+	for {
+		size, err := manifests.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		b := make([]byte, size)
+		copy(b, buf)
+		sink <- b
+	}
+	close(sink)
+}
+
+func decode(in chan []byte, out chan unstructured.Unstructured) {
+	for buf := range in {
+		spec := unstructured.Unstructured{}
+		err := yaml.NewYAMLToJSONDecoder(bytes.NewReader(buf)).Decode(&spec)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("ERROR", spec.GetName(), err)
+			}
+			continue
+		}
+		out <- spec
+	}
+	close(out)
+}
+
+func pluralize(kind string) string {
+	ret := strings.ToLower(kind)
+	switch {
+	case strings.HasSuffix(ret, "s"):
+		return fmt.Sprintf("%ses", ret)
+	case strings.HasSuffix(ret, "policy"):
+		return fmt.Sprintf("%sies", ret[:len(ret)-1])
+	default:
+		return fmt.Sprintf("%ss", ret)
+	}
+}
+
+func client(spec unstructured.Unstructured, dc dynamic.Interface) (dynamic.ResourceInterface, error) {
+	groupVersion, err := schema.ParseGroupVersion(spec.GetAPIVersion())
+	if err != nil {
+		return nil, err
+	}
+	groupVersionResource := groupVersion.WithResource(pluralize(spec.GetKind()))
+	fmt.Println(groupVersionResource)
+	if ns := spec.GetNamespace(); ns == "" {
+		return dc.Resource(groupVersionResource), nil
+	} else {
+		return dc.Resource(groupVersionResource).Namespace(ns), nil
+	}
+}
